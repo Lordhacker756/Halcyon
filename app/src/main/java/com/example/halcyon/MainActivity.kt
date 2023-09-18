8530105ef068db4cdf7e68caf0913f8eb4ea4de0
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2023-09-18 21:58:57 +0530

    Completed audio player

--- app/src/main/java/com/example/halcyon/MainActivity.kt
+++ app/src/main/java/com/example/halcyon/MainActivity.kt
@@ -3,37 +3,91 @@ package com.example.halcyon
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var mediaPlayer: MediaPlayer? = null
+    private lateinit var seekBar: SeekBar
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mediaPlayer = MediaPlayer.create(this, R.raw.music)
         val play = findViewById<FloatingActionButton>(R.id.play)
         val pause = findViewById<FloatingActionButton>(R.id.pause)
         val stop = findViewById<FloatingActionButton>(R.id.stop)
 
+        seekBar = findViewById(R.id.music_progress)
+        handler = Handler(Looper.getMainLooper())
+
         play.setOnClickListener {
-            mediaPlayer = MediaPlayer.create(this, R.raw.music)
-            mediaPlayer!!.start()
+            if(mediaPlayer == null){
+                mediaPlayer = MediaPlayer.create(this, R.raw.music)
+                initializeSeekBar()
+            }
+            mediaPlayer?.start()
             Toast.makeText(this, "Playing", Toast.LENGTH_LONG)
         }
 
         pause.setOnClickListener {
-            mediaPlayer!!.pause()
+            mediaPlayer?.pause()
             Toast.makeText(this, "Paused", Toast.LENGTH_LONG)
         }
 
-        pause.setOnClickListener {
+        stop.setOnClickListener {
             Toast.makeText(this, "Stopped", Toast.LENGTH_LONG)
-            mediaPlayer!!.stop()
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+
             mediaPlayer = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
         }
+    }
+    private fun initializeSeekBar(){
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mediaPlayer?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        var playedTime = findViewById<TextView>(R.id.start)
+        var remainingTime = findViewById<TextView>(R.id.end)
+
+        seekBar.max = mediaPlayer!!.duration
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer!!.currentPosition
+            val played = mediaPlayer!!.currentPosition / 1000
+            val total = mediaPlayer!!.duration / 1000
+            val rem = total - played
+
+            playedTime.text =  getTime(played)
+            remainingTime.text = getTime(rem)
+
+            handler.postDelayed(runnable, 1000)
+        }
+
+        handler.postDelayed(runnable, 1000)
+    }
+
+    fun getTime(time: Int): String {
+        var minutes = time / 60;
+        var seconds = time - (minutes * 60)
+        return if (seconds < 10) "$minutes:0$seconds" else "$minutes:$seconds"
     }
 }
